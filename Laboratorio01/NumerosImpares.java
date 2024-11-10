6455412b5e756427b1342ca978e78817f2eae093
Tallysson Yuri <115179564+tallyssonyuri@users.noreply.github.com>
2024-11-10 11:31:45 -0300

    Create NumerosImpares.java
    Programa básico em Java para exibir somente os números ímpares.

---
+++ Laboratorio01/NumerosImpares.java
@@ -0,0 +1,12 @@
+package com.mycompany.numerosimpares;
+
+public class NumerosImpares {
+
+    public static void main(String[] args) {
+        for (int i=1; i <=1000; i++){
+            if (i % 2 != 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
